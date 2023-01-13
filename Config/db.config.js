5f86ed9f89fe3777c2cc8394e3595436e63a2756
mhmdiamd <darmawanilham34@gmail.com>
2023-01-13 15:03:24 +0700

    deploy vol 5

--- Config/db.config.js
+++ Config/db.config.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 
 export const dbRepo = new Pool({
   user: process.env.PG_USER,
-  host: 'localhost',
+  host: 'floppy.db.elephantsql.com',
   port: process.env.PG_PORT,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_NAME,
